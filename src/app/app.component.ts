278bdd2d91e87e3ccc39e4b24fdaa5c93a394861
Karin1993 <daegumxxw@gmail.com>
2023-12-03 20:39:39 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { VincularComponent } from './vincular/vincular.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, VincularComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'CBDF_KPF_11';
 }
+
